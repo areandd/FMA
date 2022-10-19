151ae75b245cc3dfe3d8a614a498894557aff0b3
Isaiah Desta <isaiahdesta@unknown020fb5932093.attlocal.net>
2022-10-18 20:15:31 -0400

    best practice commit.

--- src/views/HomeScreen/HomeScreen.js
+++ src/views/HomeScreen/HomeScreen.js
@@ -1,12 +1,38 @@
 import React from "react";
+import "./HomeScreen.css";
 import NavBar from "../../components/NavBar/NavBar";
 import ScrollingStocks from "../../components/ScrollingStocks/ScrollingStocks";
 
 function HomeScreen() {
   return (
-    <div>
+    <div className="wrapper">
       <NavBar />
       <ScrollingStocks />
+      <div className="market__wrapper">
+        <div className="market">market</div>
+        <div className="market">market</div>
+        <div className="market">market</div>
+      </div>
+
+      <div className="social__watchlistWrapper">
+        <div className="social__posts">
+          <div className="post">
+            post
+            <div className="post__account">account</div>
+            <div className="post__content">content</div>
+          </div>
+        </div>
+
+        <div className="watchlist">
+          <div className="stock">
+            stock
+            <div className="stock__ticker">ticker</div>
+            <div className="stock__price">price</div>
+          </div>
+        </div>
+      </div>
+
+      {/* <Footer /> */}
     </div>
   );
 }
