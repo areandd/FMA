2789109592f488a887cef1e13ba306208c051d1b
Isaiah Desta <isaiahdesta@Isaiahs-MBP.attlocal.net>
2022-10-19 19:06:43 -0400

    init

---
+++ src/components/Markets/Data.js
@@ -0,0 +1,32 @@
+export const UserData = [
+    {
+        id: 1,
+        year: 2016,
+        userGain: 80000,
+        userLost: 823,
+    },
+    {
+        id: 2,
+        year: 2017,
+        userGain: 8000,
+        userLost: 8023,
+    },
+    {
+        id: 3,
+        year: 2018,
+        userGain: 800,
+        userLost: 80023,
+    },
+    {
+        id: 4,
+        year: 2019,
+        userGain: 80,
+        userLost: 800023,
+    },
+    {
+        id: 5,
+        year: 2020,
+        userGain: 8,
+        userLost: 8000023,
+    }
+]
--- src/components/Markets/Markets.js
+++ src/components/Markets/Markets.js
@@ -1,12 +1,17 @@
 import React from "react";
+import BarChart from "../BarChart";
 import "./Markets.css";
+import {} from "./"
+
 
 function Markets() {
+
+  const [userData, setUserData] = useState();
   return (
     <div>
       <div class="markets">
         <div className="market">
-          <h1>S&P 500</h1>
+          <BarChart chartData={}/>
         </div>
         <div className="market">
           <h1>NASDAQ</h1>
